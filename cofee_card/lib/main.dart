4cf0c9de70d9659f6d061479c1989a9abc32dddb
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2024-10-21 23:19:03 +0900

    add cofee_card

---
+++ cofee_card/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('My Coffee Id'),
+        backgroundColor: Colors.brown[700],
+        centerTitle: true,
+      ),
+      body: const Text('Hello, ninjas'),
+    ),
+  ));
+}
