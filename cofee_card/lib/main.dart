f93dc4d625e5fa25cf78b80dbf46e9ae8f92d376
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2024-10-22 20:54:59 +0900

    separate file

--- cofee_card/lib/main.dart
+++ cofee_card/lib/main.dart
@@ -1,35 +1,6 @@
+import 'package:cofee_card/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('My Coffee Id'),
-        backgroundColor: Colors.brown[700],
-        centerTitle: true,
-      ),
-      body: const Home(),
-    ),
-  ));
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orange,
-//      width: 200,
-//      height: 100,
-      padding: EdgeInsets.all(20),
-      margin: EdgeInsets.fromLTRB(10, 40, 0, 0),
-      child: const Text('Hello, ninja',
-          style: TextStyle(
-              fontSize: 18,
-              letterSpacing: 4,
-              decoration: TextDecoration.underline,
-              fontStyle: FontStyle.italic)),
-    );
-  }
+  runApp(const MaterialApp(home: Home()));
 }
